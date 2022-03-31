d20e6938c0133cfe76c31370d6f019d6c9df8dd3
vicky <victoria.awuor@student.moringaschool.com>
2022-03-31 23:20:16 +0300

    created the view function to validate the signup form

--- instaapp/forms.py
+++ instaapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class NewsletterForm(forms.Form):
+class SignupForm(forms.Form):
     username = forms.CharField(label='User_ame',max_length=30)
     email = forms.EmailField(label='Email')
     password = forms.PasswordInput(label='Password')
--- instaapp/views.py
+++ instaapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .forms import SignupForm
 
 # Create your views here.
+def sign_up(request):
+    if request.method=='POST':
+        form=SignupForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form=SignupForm()
+        return render (request, 'signup.html', {"form":form})
