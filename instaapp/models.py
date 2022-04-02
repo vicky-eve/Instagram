364cc62bbde037bf23efc9283eb5147677b2dfa4
vicky <victoria.awuor@student.moringaschool.com>
2022-04-02 12:01:11 +0300

    created the save, delete, and search methods

--- instaapp/models.py
+++ instaapp/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 class Image(models.Model):
@@ -13,4 +15,20 @@ class Profile(models.Model):
     bio = models.TextField(max_length=500, default='Bio', blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     photo = models.ImageField(upload_to='images/')
-    
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def save_profile(self):
+        self.user
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def search_profile(cls, user):
+        return cls.objects.filter(user__username__icontains=user).all()
+
+    def __str__(self):
+        return f'{self.user.username}'
