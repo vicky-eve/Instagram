c77fa8afbf80c2d47f195d7018e566a31fcfdc1b
vicky <victoria.awuor@student.moringaschool.com>
2022-04-02 12:17:36 +0300

    created save and delete methods for class image

--- instaapp/models.py
+++ instaapp/models.py
@@ -10,11 +10,31 @@ class Image(models.Model):
     caption = models.CharField('Caption(optional)',max_length=300, blank=True)
     date_posted = models.DateTimeField(auto_now_add=True)
     likes = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='images')
+
+    def __str__(self):
+        return f'{self.user.username}Image'
+
+    class Meta:
+        ordering = ['-date_posted']
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete
+
+    
 
 class Profile(models.Model):
     bio = models.TextField(max_length=500, default='Bio', blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     photo = models.ImageField(upload_to='images/')
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
 
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
