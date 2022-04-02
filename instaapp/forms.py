a4a10a7c91a69c38203fb83bb0a7dcda23ed6e44
vicky <victoria.awuor@student.moringaschool.com>
2022-04-02 11:19:07 +0300

    created the image model

--- instaapp/forms.py
+++ instaapp/forms.py
@@ -5,7 +5,10 @@ class SignupForm(forms.Form):
     email = forms.EmailField(label='Email')
     password = forms.PasswordInput(label='Password')
 
-class NwesletterForm(forms.Form):
+class NewsletterForm(forms.Form):
     username = forms.CharField(label='User_ame',max_length=30)
     email = forms.EmailField(label='Email')
+
+
+
    
--- instaapp/models.py
+++ instaapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=250, blank=True)
+    caption = models.CharField('Caption(optional)',max_length=300, blank=True)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
+
