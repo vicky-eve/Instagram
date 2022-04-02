a0ce0ca2832a4be6f10e83e29de32cadc1d8be05
vicky <victoria.awuor@student.moringaschool.com>
2022-04-02 11:46:43 +0300

    created the profile model

--- instaapp/models.py
+++ instaapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Image(models.Model):
@@ -8,3 +9,8 @@ class Image(models.Model):
     date_posted = models.DateTimeField(auto_now_add=True)
     likes = models.IntegerField(default=0)
 
+class Profile(models.Model):
+    bio = models.TextField(max_length=500, default='Bio', blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    photo = models.ImageField(upload_to='images/')
+    
