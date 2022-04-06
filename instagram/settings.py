ad767793d726a1b7883885bfa6c55690c2c3a601
vicky <victoria.awuor@student.moringaschool.com>
2022-04-06 10:55:23 +0300

    added the media root for the db

--- instagram/settings.py
+++ instagram/settings.py
@@ -92,8 +92,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+           'NAME': 'instas',
+        'USER': 'vicky',
+    'PASSWORD':'aderazi',
     }
 }
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Password validation
@@ -136,3 +141,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/index'
